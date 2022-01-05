87704418f60934c21f259dffdba6422be40811bd
Amman Abua <ammanabua@gmail.com>
2022-01-05 03:13:48 +0100

    implement no-input case

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".message").textContent = "Correct Number";
+
+// document.querySelector(".number").textContent = 30;
+// document.querySelector(".score").textContent = 15;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent = "⛔ No Number!"
+    }
+});
